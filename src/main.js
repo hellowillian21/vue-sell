ac121fb34c7e6ff655c2c8cf1e119fc24d756653
hellowillian21 <2544971181@qq.com>
2018-07-22 23:36:45 +0800

    third
    图片文件修改

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import goods from './components/goods/goods';
-import ratings from './components/ratings/ratings';
-import seller from './components/seller/seller';
+import VueResource from 'vue-resource'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 
 import './common/stylus/index.styl'
 
