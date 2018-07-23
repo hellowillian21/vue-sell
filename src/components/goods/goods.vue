b36fe17e512e61fe44f2523fa44f411ca338a98a
hellowillian21 <2544971181@qq.com>
2018-07-23 11:24:24 +0800

    完成商品组件滚动与联动
    完成商品组件左侧与右侧的滚动与联动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper" ref="menuWrapper">
+        <div class="menu-wrapper" ref="menuWrapper" >
             <ul>
-                <li v-for="(item,index) in goods" class="menu-item" :key="index">
+                <li v-for="(item,index) in goods" class="menu-item" :key="index" :class="{'current':currentIndex===index}"
+                    @click="selectMenu(index,$event)" ref="menuList">
                     <span class="text border-1px">
                         <span v-show="item.type>0" class="icon"
                             :class="classMap[item.type]"></span>{{item.name}}
@@ -10,9 +11,9 @@
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper" ref="foodsWrapper">
             <ul>
-                <li v-for="(item,index) in goods" :key="index" class="food-list">
+                <li v-for="(item,index) in goods" :key="index" class="food-list" ref="foodList">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
                         <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
@@ -23,12 +24,10 @@
                                 <h2 class="name">{{food.name}}</h2>
                                 <p class="desc">{{food.description}}</p>
                                 <div class="extra">
-                                    <span class="count">月售{{food.sellCount}}份</span>
-                                    <span>好评率{{food.rating}}%</span>
+                                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                                 </div>
                                 <div class="price">
-                                    <span class="now">￥{{food.price}}</span>
-                                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                                    <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                                 </div>
                             </div>
                         </li>
@@ -49,24 +48,81 @@
         },
         data() {
             return {
-                goods: []
+                goods: [],
+                listHeight: [], //存储每个区间的高度
+                scrollY: 0
+            }
+        },
+        computed: {
+            currentIndex() {
+                for (let i = 0; i < this.listHeight.length; i++) {
+                    let height1 = this.listHeight[i];
+                    let height2 = this.listHeight[i + 1];
+                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                        // this._followScroll(i);
+                        return i;
+                    }
+                }
+                return 0;
             }
         },
         created() {
-        this.classMap = ['decrease', 'discount', 'special','invoice', 'guarantee']
+            this.classMap = ['decrease', 'discount', 'special','invoice', 'guarantee']
             this.$http.get('api/goods').then((response) => {
                 response = response.body
                 if (response.errno === ERR_OK) {
                     this.goods = response.data
                     // console.log(this.goods)
+                    this.$nextTick(() => {
+                        this._initScroll()
+                        this._calculateHeight()
+                    })
                 }
             })
         },
         methods: {
+            //点击左侧菜单
+            selectMenu(index, event) {
+                console.log(event)
+                if (!event._constructed) {
+                    return;
+                }
+                let foodList = this.$refs.foodList;
+                let el = foodList[index];
+                this.foodsScroll.scrollToElement(el, 300);
+            },
+
             _initScroll() {
                 this.meunScroll = new BScroll(this.$refs.menuWrapper, {
                     click: true
                 });
+                // console.log(this.meunScroll)
+                this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+                    click: true,
+                    probeType: 3
+                });
+
+                this.foodsScroll.on('scroll', (pos) => {
+                    // console.log(pos)
+                    // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+                    if (pos.y <= 0) {
+                        // console.log(111)
+                        this.scrollY = Math.abs(Math.round(pos.y));
+                        // console.log(this.scrollY)
+                    }
+                });
+            },
+
+            _calculateHeight() {
+                let foodList = this.$refs.foodList
+                console.log(foodList)
+                let height = 0;
+                this.listHeight.push(height);
+                for (let i = 0; i < foodList.length; i++) {
+                    let item = foodList[i];
+                    height += item.clientHeight;
+                    this.listHeight.push(height);
+                }
             }
         }
     }
@@ -91,6 +147,14 @@
                 width 56px
                 line-height 14px
                 padding 0 12px
+                &.current
+                    position relative
+                    margin-top -1px
+                    z-index 10
+                    background #ffffff
+                    font-weight 700
+                    .text
+                        border-none()
                 .icon
                     display inline-block
                     vertical-align top
@@ -150,8 +214,9 @@
                         color rgb(147, 153, 159)
                     .desc
                         margin-bottom  8px
+                        line-height 12px
                     .extra
-                        &.count
+                        .count
                             margin-right 12px
                     .price
                         font-weight 700
