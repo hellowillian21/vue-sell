2ed78b2023cea422bfafbf78a57d54721ee0d231
hellowillian21 <2544971181@qq.com>
2018-07-25 14:47:08 +0800

    完成cartcontrol组件&&关联goods组件与shopcart组件
    完成cartcontrol组件&&关联goods组件与shopcart组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -29,19 +29,23 @@
                                 <div class="price">
                                     <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                                 </div>
+                                <div class="cartcontrol-wrapper">
+                                    <cartcontrol :food="food"></cartcontrol>
+                                </div>
                             </div>
                         </li>
                     </ul>
                 </li>
             </ul>
         </div>
-        <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+        <shopcart :selectFoods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
     </div>
 </template>
 
 <script>
     import BScroll from 'better-scroll'
     import shopcart from '../../components/shopcart/shopcart'
+    import cartcontrol from '../../components/cartcontrol/cartcontrol'
 
     const ERR_OK = 0
     export default {
@@ -66,6 +70,17 @@
                     }
                 }
                 return 0;
+            },
+            selectFoods() {
+                let foods = []
+                this.goods.forEach((good) => {
+                    good.foods.forEach((food) => {
+                        if (food.count) {
+                            foods.push(food)
+                        }
+                    })
+                })
+                return foods
             }
         },
         created() {
@@ -128,7 +143,8 @@
             }
         },
         components: {
-            shopcart
+            shopcart,
+            cartcontrol
         }
     }
 </script>
@@ -206,6 +222,7 @@
                     flex 0 0 57px
                     margin-right 10px
                 .content
+                    position relative
                     flex 1
                     .name
                         margin 2px 0 8px 0
@@ -234,4 +251,8 @@
                             text-decoration line-through
                             font-size 10px
                             color rgb(147, 153, 159)
+                    .cartcontrol-wrapper
+                        position absolute
+                        right 0
+                        bottom 12px
 </style>            
